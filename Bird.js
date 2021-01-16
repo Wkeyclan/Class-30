7a069fa08d7e72b498855de153129d107a21a00d
Wkeyclan <67618880+Wkeyclan@users.noreply.github.com>
2021-01-16 16:42:02 -0500

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+    constructor(x, y) {
+        /* super() helps to transfer properties of parent class through 
+        parent class constructor*/
+        super(x, y, 50, 50);
+        this.image = loadImage("sprites/bird.png");
+        this.visibility = 255;
+    }
+
+    display() {
+        if (this.body.speed < 3) {
+            super.display();
+        } else {
+            World.remove(world, this.body);
+            push()
+            this.visibility = this.visibility - 15;
+            tint(255, this.visibility);
+            image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+            pop()
+        }
+    }
+}
